3b9c3a6bb8f1776aa9246d459b910f38746bc829
GDS83192 <graham.squires3@gmail.com>
2018-01-29 18:44:40 -0500

    stuff

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
     remoteUrl : 'mongodb://graham:GDS831@ds157614.mlab.com:57614/graham',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+   // localUrl: 'mongodb://localhost/meanstacktutorials'
 };
